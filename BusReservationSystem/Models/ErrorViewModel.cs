c4ae96785ea0b258187c47fe986a12b53ea34f1c
Muskan Mushtaq <muskan2006955@gmail.com>
2026-03-25 09:17:48 -0700

    Cleanup: Removed ignored files and folders

--- BusReservationSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BusReservationSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
